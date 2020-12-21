6606a047180f4f1fd0d2748c2fa92d3d16495095
giadarol <giovanni.iadarola@cern.ch>
2020-12-21 17:24:17 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='hellofrom',
-    packages=find_packages()
+    packages=find_packages(),
     install_requires=[
         'numpy>=1.0',
         ]
-        :q
     )
