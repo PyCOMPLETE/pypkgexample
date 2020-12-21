d4444b20f6647d21c33c2b7e1b07e5d3ee727f7d
giadarol <giovanni.iadarola@cern.ch>
2020-12-21 22:07:32 +0100

    Status

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
 
+from numpy.distutils.core import Extension as npExtension
+from numpy.distutils.core import setup
+
+ext1 = npExtension(
+        name = 'hffortran',
+        sources = ['./hellofrom/hello_from_fortran/sqrt_array.f90'])
+
 setup(
     name='hellofrom',
     packages=find_packages(),
+    ext_modules = [ext1],
     install_requires=[
         'numpy>=1.0',
         'pytest', # This could be made optional but we don't do it 
