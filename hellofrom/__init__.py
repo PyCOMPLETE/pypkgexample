d84ebe04d8b7fc8eaa6de5b174f400a9fdad514c
giadarol <giovanni.iadarola@cern.ch>
2020-12-22 14:41:21 +0100

    Updated init and tests

--- hellofrom/__init__.py
+++ hellofrom/__init__.py
@@ -1,2 +1,6 @@
 from .hellofrom_python.hello import say_hello as say_hello_python
 from .hellofrom_python.hello import sqrt_array as sqrt_array_python
+
+from .hellofrom_c_with_ctypes.hello import sqrt_array as sqrt_array_c_ctypes
+
+
--- tests/test_sqrts.py
+++ tests/test_sqrts.py
@@ -5,3 +5,8 @@ def test_sqrt_python():
     assert np.max(
             hf.sqrt_array_python([1., 4., 9.])-
           -  np.array([1., 2., 3.])) < 1e30
+
+def test_sqrt_c_ctypes():
+    assert np.max(
+            hf.sqrt_array_c_ctypes([1., 4., 9.])-
+          -  np.array([1., 2., 3.])) < 1e30
