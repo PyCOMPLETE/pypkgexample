b3f79d57cab239b3dc8e370ce2a131cb7341e925
giadarol <giovanni.iadarola@cern.ch>
2020-12-23 18:18:02 +0100

    Fixed examples

--- examples/sqrts_from_everybody.py
+++ examples/sqrts_from_everybody.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-import hellofrom as hf
+import pypkgexample as pe
 
 a = np.array([9., 4., 1.])
 
-print(f'From python: {hf.sqrt_array_python(a)}')
-print(f'From Fortran via f2py: {hf.sqrt_array_fortran(a)}')
-print(f'From C via ctypes: {hf.sqrt_array_c_ctypes(a)}')
-print(f'From C via cython: {hf.sqrt_array_c_cython(a)}')
+print(f'From python: {pe.sqrt_array_python(a)}')
+print(f'From Fortran via f2py: {pe.sqrt_array_fortran(a)}')
+print(f'From C via ctypes: {pe.sqrt_array_c_ctypes(a)}')
+print(f'From C via cython: {pe.sqrt_array_c_cython(a)}')
