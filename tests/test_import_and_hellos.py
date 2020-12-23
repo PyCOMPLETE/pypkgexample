3cf5e55c25483d996c990e5b1e1ab51a0e391eb3
giadarol <giovanni.iadarola@cern.ch>
2020-12-23 18:16:41 +0100

    Fixed tests

--- tests/test_import_and_hellos.py
+++ tests/test_import_and_hellos.py
@@ -1,13 +1,13 @@
-import hellofrom as hf
+import pypkgexample as pe
 
 def test_python():
-    hf.say_hello_python()
+    pe.say_hello_python()
 
 def test_fortran():
-    hf.say_hello_fortran()
+    pe.say_hello_fortran()
 
 def test_c_ctypes():
-    hf.say_hello_c_ctypes()
+    pe.say_hello_c_ctypes()
 
 def test_c_cython():
-    hf.say_hello_c_cython()
+    pe.say_hello_c_cython()
--- tests/test_sqrts.py
+++ tests/test_sqrts.py
@@ -1,22 +1,22 @@
-import hellofrom as hf
+import pypkgexample as pe
 import numpy as np
 
 def test_sqrt_python():
     assert np.max(
-            hf.sqrt_array_python([1., 4., 9.])-
+            pe.sqrt_array_python([1., 4., 9.])-
           -  np.array([1., 2., 3.])) < 1e30
 
 def test_sqrt_fortran():
     assert np.max(
-            hf.sqrt_array_fortran([1., 4., 9.])-
+            pe.sqrt_array_fortran([1., 4., 9.])-
           -  np.array([1., 2., 3.])) < 1e30
 
 def test_sqrt_c_ctypes():
     assert np.max(
-            hf.sqrt_array_c_ctypes([1., 4., 9.])-
+            pe.sqrt_array_c_ctypes([1., 4., 9.])-
           -  np.array([1., 2., 3.])) < 1e30
 
 def test_sqrt_c_cython():
     assert np.max(
-            hf.sqrt_array_c_cython([1., 4., 9.])-
+            pe.sqrt_array_c_cython([1., 4., 9.])-
           -  np.array([1., 2., 3.])) < 1e30
