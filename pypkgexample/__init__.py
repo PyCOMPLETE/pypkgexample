b50764211dd24e9243ff2f943b266ae4094e4631
giadarol <giovanni.iadarola@cern.ch>
2020-12-26 14:09:36 +0100

    Missed file

--- pypkgexample/__init__.py
+++ pypkgexample/__init__.py
@@ -9,6 +9,5 @@ from .mymodule_fortran.hello import say_hello as say_hello_fortran
 from .mymodule_c_with_ctypes.hello import sqrt_array as sqrt_array_c_ctypes
 from .mymodule_c_with_ctypes.hello import say_hello as say_hello_c_ctypes
 
-
 from .mymodule_c_with_cython.hello import sqrt_array as sqrt_array_c_cython
 from .mymodule_c_with_cython.hello import say_hello as say_hello_c_cython
